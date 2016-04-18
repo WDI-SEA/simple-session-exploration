63e3233ba6d08f41929d5a159bab8532b3a8baa2
Steve Geluso <stevegeluso@gmail.com>
2016-04-18 16:37:04 -0700

    cool session app

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var session = require("express-session");
+
+var app = express();
+app.use(session({
+  secret: 'mooncheese',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get("/", function(req, res) {
+  // pick a random number from 0-9
+  var ran = Math.floor(Math.random() * 10);
+
+  // set the value of the session at that number to a random decimal.
+  req.session[ran] = Math.random();
+
+  // send the entire session back
+  res.send(req.session);
+});
+
+app.listen(3000);

3fb63edb110b936c3e4a9ac4bcf398bc51443bca
Steve Geluso <stevegeluso@gmail.com>
2016-04-19 14:17:45 -0700

    ..

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var session = require("express-session");
+var bcrypt = require("bcrypt");
 
 var app = express();
 app.use(session({
@@ -8,6 +9,13 @@ app.use(session({
   saveUninitialized: true
 }));
 
+app.use(function(req, res, next) {
+  req.getUser = function() {
+    return session.user;
+  }
+  next();
+});
+
 app.get("/", function(req, res) {
   // pick a random number from 0-9
   var ran = Math.floor(Math.random() * 10);
@@ -15,8 +23,13 @@ app.get("/", function(req, res) {
   // set the value of the session at that number to a random decimal.
   req.session[ran] = Math.random();
 
-  // send the entire session back
-  res.send(req.session);
+  bcrypt.hash("secretpass", 10, function(err, hash) {
+    // add the crypted password to the session
+    req.session.password = hash;
+
+    // send the entire session back
+    res.send(req.session);
+  });
 });
 
 app.listen(3000);
